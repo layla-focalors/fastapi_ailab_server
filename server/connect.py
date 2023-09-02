bfb55f531e50d18e63dd7b71d5d575d39d208e14
arisia <starlights2283@gmail.com>
2023-09-02 22:07:30 +0900

    add fileS

---
+++ server/connect.py
@@ -0,0 +1,17 @@
+def connect(database, useremail, userpassword):
+    import datetime
+    import pymysql
+    try:
+        connection = pymysql.connect(host='localhost', user='root', password='root', database='login',cursorclass=pymysql.cursors.DictCursor)
+    except:
+        return "connection error"
+    time = str(datetime.datetime.now())
+    querry = f"INSERT INTO register VALUES({time}, {useremail}, {userpassword});"
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(querry)
+            connection.commit()
+            connection.close()
+            return "querry success"
+    except:
+        return "querry error"
